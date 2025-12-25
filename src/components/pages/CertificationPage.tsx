85b1a6786711cd2b7f46f24da3cb94cd7bcfe700
Michael Daross <github@crookedgrin.com>
2025-12-25 17:15:52 -0600

    Update CertificationPage.tsx

--- src/components/pages/CertificationPage.tsx
+++ src/components/pages/CertificationPage.tsx
@@ -1,4 +1,5 @@
 import { PageContent } from '@components/PageContent'
+import { Box, Image, Stack } from '@mantine/core'
 import type { PrismicDocument } from '@prismicio/client'
 import type { Slice } from '@slices'
 import { SliceZone } from '@slices'
@@ -13,35 +14,26 @@ export function CertificationPage({ page }: Props) {
 
   return (
     <PageContent>
-      <article className="prose sm:prose-lg xl:prose-xl mx-auto max-w-4xl xl:max-w-5xl">
+      <Box className="mx-auto rounded-2xl bg-white p-12">
         {/* Certification badge */}
-        {certificationImg?.url ? (
-          <img src={certificationImg.url} alt="Fossil-free certified badge" className="h-48 w-48" />
-        ) : (
-          <img
-            src="/img/certification/fossil-free-certified.png"
-            alt="Fossil-free certified badge"
-            className="h-48 w-48"
-          />
-        )}
-
+        <Box className="mb-6 md:float-right md:ml-6">
+          {certificationImg?.url ? (
+            <Image
+              src={certificationImg.url}
+              alt="Fossil-free certified badge"
+              className="h-48 w-48"
+            />
+          ) : (
+            <Image
+              src="/img/certification/fossil-free-certified.png"
+              alt="Fossil-free certified badge"
+              className="h-48 w-48"
+            />
+          )}
+        </Box>
         {/* Main content from Prismic slices */}
-        {slices ? (
-          <SliceZone slices={slices} />
-        ) : (
-          <>
-            <h1>Fossil Free Certification</h1>
-            <p>
-              Financial institutions that are Fossil Free Certified have pledged not to finance new
-              fossil fuel companies or projects, and the majority already do not do so. Fossil Free
-              Certification is the simplest, most straightforward way to signal to customers,
-              professionals in the banking sector, and the general public that a
-              sustainability-conscious financial institution is truly putting its money where its
-              mouth is.
-            </p>
-          </>
-        )}
-      </article>
+        <Stack className="gap-6">{slices && <SliceZone slices={slices} />}</Stack>
+      </Box>
     </PageContent>
   )
 }
