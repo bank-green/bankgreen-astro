eca94708b1ce2c7cb87b2ee36cdb917b06878723
Einar <einar.skreslett@gmail.com>
2026-08-14 18:48:39 +0100

    updated TypeformEmbed to accept query params

--- src/components/TypeformEmbed.tsx
+++ src/components/TypeformEmbed.tsx
@@ -3,18 +3,27 @@ import * as typeformEmbed from '@typeform/embed'
 import { useEffect, useRef } from 'react'
 import { theme } from '@/styles/theme'
 
-export function TypeformEmbed() {
+const DEFAULT_TYPEFORM_ID = 'rwxTfN15'
+
+export function TypeformEmbed({
+  formId = DEFAULT_TYPEFORM_ID,
+  transitiveSearchParams,
+}: {
+  formId?: string
+  transitiveSearchParams?: readonly string[]
+}) {
   const containerRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     if (containerRef.current) {
-      typeformEmbed.createWidget('rwxTfN15', {
+      typeformEmbed.createWidget(formId, {
         container: containerRef.current,
         width: '100%',
         height: 800,
+        ...(transitiveSearchParams ? { transitiveSearchParams: [...transitiveSearchParams] } : {}),
       })
     }
-  }, [])
+  }, [formId, transitiveSearchParams])
 
   return (
     <MantineProvider theme={theme}>
